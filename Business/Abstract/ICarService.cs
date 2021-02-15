19a8045ebad345a29053ce26c0a7f68e3bea5f5a
sümeyra sağır <sumeyrasagir2017@outlook.com>
2021-02-15 21:42:31 +0300

    8. ders sonu

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -7,6 +7,11 @@ namespace Business.Abstract
 {
    public interface ICarService
     {
+        void Add(Car car);
+        void Update(Car car);
+        void Delete(Car car);
         List<Car> GetAll();
+        List<Car> GetCarsByBrandId(int brandId);
+        List<Car> GetCarsByColorId(int colorId);
     }
 }
--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -16,9 +16,51 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        public void Add(Car car)
+        {
+            if (car.Description.Length>=2 && car.DailyPrice>0)
+            {
+                _carDal.Add(car);
+                Console.WriteLine("Araba eklendi");
+            }
+            else
+            {
+                Console.WriteLine("Günlük ücret 0'dan büyük olmalı ve Araba ismi en az iki karakterden oluşmalıdır !");
+            }
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+            Console.WriteLine("Araba silindi");
+        }
+
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int brandId)
+        {
+            return _carDal.GetAll(c => c.BrandId == brandId);
+        }
+
+        public List<Car> GetCarsByColorId(int colorId)
+        {
+            return _carDal.GetAll(c => c.ColorId == colorId);
+        }
+
+        public void Update(Car car)
+        {
+            if (car.Description.Length >= 2 && car.DailyPrice > 0)
+            {
+                _carDal.Add(car);
+                Console.WriteLine("Araba güncellendi");
+            }
+            else
+            {
+                Console.WriteLine("Günlük ücret 0'dan büyük olmalı ve Araba ismi en az iki karakterden oluşmalıdır !");
+            }
         }
     }
 }
